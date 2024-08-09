520a58f3e3573ae705cb2140979be7b9d17c4441
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-08-09 18:37:30 -0300

    Movement and Jump added.
    Added the movement and jump logic for the character navigate through the board.
    
    Adicionada lógica de movimentação e de pulo para o personagem navegar através do tabuleiro.

--- turnBasedRPG/Assets/Scripts/Board/Movement.cs
+++ turnBasedRPG/Assets/Scripts/Board/Movement.cs
@@ -7,9 +7,12 @@ public class Movement : MonoBehaviour
     public bool teste;
     public Vector3Int destino;
     SpriteRenderer SR;
+    Transform jumper;
+    TileLogic tileAtual;
 
-    private void Start()
+    private void Awake()
     {
+        jumper = transform.Find("Jumper");
         SR = GetComponentInChildren<SpriteRenderer>();
     }
 
@@ -27,8 +30,52 @@ public class Movement : MonoBehaviour
     {
         yield return null;
         TileLogic t = Board.instance.tiles[destino];
-        transform.position = t.worldPos;
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = t.worldPos;
+        float totalTime=1;
+        float tempTime=0;
+
+        if(tileAtual == null)
+            tileAtual = t;
+        if (tileAtual.floor != t.floor)
+            StartCoroutine(Jump(t, totalTime));
+
+        while(transform.position != endPos)
+        {
+            tempTime += Time.deltaTime;
+            float perc = tempTime / totalTime;
+            transform.position = Vector3.Lerp(startPos, endPos, perc);
+            yield return null;
+        }
+
+
         SR.sortingOrder = t.contentOrder;
         t.content = this.gameObject;
+    }
+
+    IEnumerator Jump(TileLogic t, float totalTime)
+    {
+        Vector3 halfwayPos;
+        Vector3 startpos = halfwayPos = jumper.localPosition;
+        halfwayPos.y += 0.5f;
+        float tempTime = 0;
+
+        while(jumper.localPosition != halfwayPos)
+        {
+            tempTime += Time.deltaTime;
+            float perc = tempTime / (totalTime / 2);
+            jumper.localPosition = Vector3.Lerp(startpos, halfwayPos, perc);
+            yield return null;
+        }
+
+        tempTime = 0;
+        while(jumper.localPosition != startpos)
+        {
+            tempTime += Time.deltaTime;
+            float perc = tempTime / (totalTime / 2);
+            jumper.localPosition = Vector3.Lerp(halfwayPos, startpos, perc);
+            yield return null;
+        }
     }
 }

fe5fdce62633239bff2fd114d896de6b741a08f6
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-09-02 22:09:06 -0300

    Stats bug fixed.
    Fixed a bug where a unit would not load all the stats correctly.
    
    Corrigido um erro onde uma unidade não carregaria todos os atributos corretamente.

--- turnBasedRPG/Assets/Scripts/Unit/Stats/Stats.cs
+++ turnBasedRPG/Assets/Scripts/Unit/Stats/Stats.cs
@@ -6,12 +6,13 @@ public class Stats : MonoBehaviour
 {
     public List<Stat> stats;
 
-    private void Awake()
+     void Awake()
     {
         stats = new List<Stat>();
-        for (int i=0; i<10; i++){
+        for (int i = 0; i < 10; i++)
+        {
             Stat temp = new Stat();
-            temp.type = (StatEnum)1;
+            temp.type = (StatEnum)i;
             stats.Add(temp);
         }
     }
